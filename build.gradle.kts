9df374cfa15c451d1bdd755bafe1aff18bcfb192
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-06 15:58:27 +0300

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.3.70"
+    id("io.gitlab.arturbosch.detekt") version "1.6.0"
 }
 
 group = "org.example"
@@ -7,10 +8,22 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.70")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+}
+
+detekt {
+    failFast = true // fail build on any finding
+    buildUponDefaultConfig = true // preconfigure defaults
 }
 
 tasks {
@@ -19,5 +32,8 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
     }
 }
