91af0d5aaa40650612a4a3a07c09fe9bada3d284
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-08 21:42:04 +0300

    Code refactoring

--- src/main/kotlin/homework/homework1/task1/OperatingSystem.kt
+++ src/main/kotlin/homework/homework1/task1/OperatingSystem.kt
@@ -1,11 +1,13 @@
 package homework.homework1.task1
 
-private const val WINDOWS_PROBABILITY_OF_INFECTION = 0.8
-private const val MACOS_PROBABILITY_OF_INFECTION = 0.5
-private const val LINUX_PROBABILITY_OF_INFECTION = 0.1
+private object ProbabilityOfInfection {
+    const val WINDOWS = 0.8 // The probability is 80%
+    const val MACOS = 0.5 // The probability is 50%
+    const val LINUX = 0.1 // The probability is 10%
+}
 
 enum class OperatingSystem(val probabilityOfInfection: Double) {
-    WINDOWS(WINDOWS_PROBABILITY_OF_INFECTION), // The probability is 80%
-    MACOS(MACOS_PROBABILITY_OF_INFECTION), // The probability is 50%
-    LINUX(LINUX_PROBABILITY_OF_INFECTION) // The probability is 10%
+    WINDOWS(ProbabilityOfInfection.WINDOWS),
+    MACOS(ProbabilityOfInfection.MACOS),
+    LINUX(ProbabilityOfInfection.LINUX)
 }
