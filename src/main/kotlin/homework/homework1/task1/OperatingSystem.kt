fe12a368d86ddf4d3deabc6d183791d8e4b43ac9
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-08 18:44:57 +0300

    Fixed detekt issue

--- src/main/kotlin/homework/homework1/task1/OperatingSystem.kt
+++ src/main/kotlin/homework/homework1/task1/OperatingSystem.kt
@@ -1,7 +1,11 @@
 package homework.homework1.task1
 
+private const val WINDOWS_PROBABILITY_OF_INFECTION = 0.8
+private const val MACOS_PROBABILITY_OF_INFECTION = 0.5
+private const val LINUX_PROBABILITY_OF_INFECTION = 0.1
+
 enum class OperatingSystem(val probabilityOfInfection: Double) {
-    WINDOWS(0.8), // The probability is 80%
-    MACOS(0.5), // The probability is 50%
-    LINUX(0.1) // The probability is 10%
+    WINDOWS(WINDOWS_PROBABILITY_OF_INFECTION), // The probability is 80%
+    MACOS(MACOS_PROBABILITY_OF_INFECTION), // The probability is 50%
+    LINUX(LINUX_PROBABILITY_OF_INFECTION) // The probability is 10%
 }
