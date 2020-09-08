e28956674076bd88c88d12d9ff6ed1f11f4c882c
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-08 18:41:19 +0300

    Added some tests

--- src/test/kotlin/homework/homework1/task1/SimulationTest.kt
+++ src/test/kotlin/homework/homework1/task1/SimulationTest.kt
@@ -46,4 +46,28 @@ internal class SimulationTest {
         val isComputer4Infected = test.viruses[0].infectedComputers.contains(test.computers[3])
         assert(isComputer1Infected && !isComputer3Infected && !isComputer4Infected)
     }
+
+    @Test
+    fun run_5Computers_checkComputers_mustWork() {
+        val test = Simulation()
+        test.import(File("./src/test/resources/kotlin/homework/homework1/task1/5Computers.txt"))
+        test.run(1, 0)
+        val isComputer1Infected = test.computers[0].viruses.contains("WannaCry")
+        val isComputer3Infected = test.computers[2].viruses.contains("WannaCry")
+        val isComputer4Infected = test.computers[3].viruses.contains("WannaCry")
+        val isComputer5Infected = test.computers[4].viruses.contains("WannaCry")
+        assert(isComputer1Infected && !isComputer3Infected && !isComputer4Infected && !isComputer5Infected)
+    }
+
+    @Test
+    fun run_5Computers_checkViruses_mustWork() {
+        val test = Simulation()
+        test.import(File("./src/test/resources/kotlin/homework/homework1/task1/5Computers.txt"))
+        test.run(1, 0)
+        val isComputer1Infected = test.viruses[0].infectedComputers.contains(test.computers[0])
+        val isComputer3Infected = test.viruses[0].infectedComputers.contains(test.computers[2])
+        val isComputer4Infected = test.viruses[0].infectedComputers.contains(test.computers[3])
+        val isComputer5Infected = test.viruses[0].infectedComputers.contains(test.computers[4])
+        assert(isComputer1Infected && !isComputer3Infected && !isComputer4Infected && !isComputer5Infected)
+    }
 }
