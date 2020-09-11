03cb280da6462d961460d1a203a3bcc6c29b52bd
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-11 19:11:37 +0300

    Replaced OS class and probabilityOfInfection object with single OS dataclass

--- src/main/kotlin/homework/homework1/task1/OperatingSystem.kt
+++ src/main/kotlin/homework/homework1/task1/OperatingSystem.kt
@@ -1,13 +1,3 @@
 package homework.homework1.task1
 
-private object ProbabilityOfInfection {
-    const val WINDOWS = 0.8 // The probability is 80%
-    const val MACOS = 0.5 // The probability is 50%
-    const val LINUX = 0.1 // The probability is 10%
-}
-
-enum class OperatingSystem(val probabilityOfInfection: Double) {
-    WINDOWS(ProbabilityOfInfection.WINDOWS),
-    MACOS(ProbabilityOfInfection.MACOS),
-    LINUX(ProbabilityOfInfection.LINUX)
-}
+data class OperatingSystem(val name: String, val probabilityOfInfection: Double)
