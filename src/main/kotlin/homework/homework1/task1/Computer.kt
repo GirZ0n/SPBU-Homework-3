5a798d4d4b5bcd6aae074250f6776bcff734717a
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-12 14:58:42 +0300

    Computer class refactoring 1) Changed printStatistics function to toString; 2) Now installVirus function doesn't return Boolean; 3) Changed id to name; 4) Removed connectedComputers field.

--- src/main/kotlin/homework/homework1/task1/Computer.kt
+++ src/main/kotlin/homework/homework1/task1/Computer.kt
@@ -2,27 +2,17 @@ package homework.homework1.task1
 
 import kotlin.random.Random
 
-class Computer(private val id: Int, private val operatingSystem: OperatingSystem) {
-    val connectedComputers = emptyList<Computer>().toMutableList()
-    val viruses = emptyList<String>().toMutableList()
+class Computer(private val name: String, private val os: OperatingSystem, val viruses: MutableList<Virus>) {
+    override fun toString(): String =
+            "$name (${os.name}): " +
+                    if (viruses.isNotEmpty()) viruses.joinToString(", ") { it.name } else "not infected"
 
-    fun printStatistics() {
-        println(
-            when (viruses.isEmpty()) {
-                true -> "Device №$id (${operatingSystem.name}): not infected"
-                false -> "Device №$id (${operatingSystem.name}): ${viruses.joinToString(", ")}"
-            }
-        )
-    }
+    fun isInfectedBy(virus: Virus) = viruses.contains(virus)
 
-    fun isInfectedBy(virusName: String) = viruses.contains(virusName)
-
-    fun installVirus(virusName: String): Boolean {
-        val randomNumber = Random.nextDouble(0.0, 1.0)
-        if (!isInfectedBy(virusName) && operatingSystem.probabilityOfInfection > randomNumber) {
-            viruses.add(virusName)
-            return true
+    fun installVirus(virus: Virus) {
+        val randomNumber = Random.nextDouble()
+        if (!isInfectedBy(virus) && os.probabilityOfInfection > randomNumber) {
+            viruses.add(virus)
         }
-        return false
     }
 }
