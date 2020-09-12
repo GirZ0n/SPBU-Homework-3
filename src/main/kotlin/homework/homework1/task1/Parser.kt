6cd24bc2289d7532c1262ad9dfeb11c3051a1919
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-12 16:17:13 +0300

    Fixed require()

--- src/main/kotlin/homework/homework1/task1/Parser.kt
+++ src/main/kotlin/homework/homework1/task1/Parser.kt
@@ -9,7 +9,7 @@ class Parser(file: File) {
     fun getNumberOfComputers(): Int {
         val line = scan.nextLine()
         val numberOfComputers = line.toInt()
-        require(numberOfComputers <= 0) { "The number must be greater than 0. \nProblem line: $line" }
+        require(numberOfComputers > 0) { "The number must be greater than 0. \nProblem line: $line" }
         return numberOfComputers
     }
 
@@ -19,7 +19,7 @@ class Parser(file: File) {
             val line = scan.nextLine()
             network[i] = line.split(' ').map { it.toInt() }.toIntArray()
             for (currentNumber in network[i]) {
-                require(currentNumber < 0 || currentNumber > 1) {
+                require(currentNumber == 0 || currentNumber == 1) {
                     "The number must be either 0 or 1. \nProblem line: $line"
                 }
             }
@@ -36,7 +36,7 @@ class Parser(file: File) {
         for (i in 0 until numberOfComputers) {
             val line = scan.nextLine()
             val probability = line.toDouble()
-            require(probability < 0.0 || probability > 1.0) {
+            require(probability in 0.0..1.0) {
                 "The probability must be between 0 and 1. \nProblem line: $line"
             }
             computersOSProbabilityOfInfection[i] = probability
@@ -51,7 +51,7 @@ class Parser(file: File) {
     fun getNumberOfViruses(): Int {
         val line = scan.nextLine()
         val numberOfViruses = line.toInt()
-        require(numberOfViruses <= 0) { "The number must be greater than 0. \nProblem line: $line" }
+        require(numberOfViruses > 0) { "The number must be greater than 0. \nProblem line: $line" }
         return numberOfViruses
     }
 
@@ -69,7 +69,7 @@ class Parser(file: File) {
             val line = scan.nextLine()
             val infectedComputers = line.split(' ').map { it.toInt() }.toList()
             for (currentNumber in infectedComputers) {
-                require(currentNumber <= 0 || currentNumber > numberOfComputers) {
+                require(currentNumber in 1..numberOfComputers) {
                     "The number must be greater than 0 and less or equal than $numberOfComputers. \nProblem line: $line"
                 }
             }
