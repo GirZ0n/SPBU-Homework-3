707464f8abc1577c20a1a8593c58a38a0aaf0aad
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-12 15:52:14 +0300

    Small code refactoring

--- src/main/kotlin/homework/homework1/task1/Computer.kt
+++ src/main/kotlin/homework/homework1/task1/Computer.kt
@@ -2,9 +2,11 @@ package homework.homework1.task1
 
 import kotlin.random.Random
 
-class Computer(private val name: String, private val os: OperatingSystem, val viruses: MutableList<Virus>) {
+class Computer(private val name: String, private val os: OperatingSystem) {
+    val viruses = mutableListOf<Virus>()
+
     override fun toString(): String = "$name (${os.name}): " +
-                    if (viruses.isNotEmpty()) viruses.joinToString(", ") { it.name } else "not infected"
+            if (viruses.isNotEmpty()) viruses.joinToString(", ") { it.name } else "not infected"
 
     fun isInfectedBy(virus: Virus) = viruses.contains(virus)
 
