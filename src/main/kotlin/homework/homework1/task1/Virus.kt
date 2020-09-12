1832351635e33ef5470c49dccef13803a4bfdbfd
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-09-12 15:00:06 +0300

    Virus class refactoring: 1) Now it's data class; 2) Now Virus knows only his name.

--- src/main/kotlin/homework/homework1/task1/Virus.kt
+++ src/main/kotlin/homework/homework1/task1/Virus.kt
@@ -1,19 +1,3 @@
 package homework.homework1.task1
 
-class Virus(val name: String) {
-    val infectedComputers = emptyList<Computer>().toMutableList()
-
-    fun spread() {
-        val newInfectedComputers = mutableListOf<Computer>()
-        for (infectedComputer in infectedComputers) {
-            for (computer in infectedComputer.connectedComputers) {
-                if (tryToInfect(computer)) {
-                    newInfectedComputers += computer
-                }
-            }
-        }
-        infectedComputers += newInfectedComputers
-    }
-
-    private fun tryToInfect(computer: Computer) = !computer.isInfectedBy(name) && computer.installVirus(name)
-}
+data class Virus(val name: String)
