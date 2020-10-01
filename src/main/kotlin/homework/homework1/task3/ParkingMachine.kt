5eb58c19986d18183b16e1b16b64f148b167261e
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-10-01 23:18:49 +0300

    Fixed Detekt issues

--- src/main/kotlin/homework/homework1/task3/ParkingMachine.kt
+++ src/main/kotlin/homework/homework1/task3/ParkingMachine.kt
@@ -1,8 +1,5 @@
 package homework.homework1.task3
 
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-
 class ParkingMachine(parking: Parking) {
     private val request = RequestServer(parking)
 
