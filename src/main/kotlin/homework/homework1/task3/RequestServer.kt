04be80a01a0ad53c1b3b8f2e289d974008eeb72e
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-10-01 23:21:32 +0300

    Fixed Detekt issues

--- src/main/kotlin/homework/homework1/task3/RequestServer.kt
+++ src/main/kotlin/homework/homework1/task3/RequestServer.kt
@@ -1,9 +1,5 @@
 package homework.homework1.task3
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.launch
-
 class RequestServer(private val parking: Parking) {
     fun tryToEnter() = parking.tryToEnter()
 
